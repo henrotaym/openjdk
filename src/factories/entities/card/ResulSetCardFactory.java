415feb18bf8f08b982fe64e7ef6a55ed05aed3a7
Zer0-lab <kevin.dhoust@gmail.com>
2025-01-30 13:21:54 +0000

    Ajout d'une colonne status dans la DB et modifcation de la classe Card

--- src/factories/entities/card/ResulSetCardFactory.java
+++ src/factories/entities/card/ResulSetCardFactory.java
@@ -1,18 +1,21 @@
 package factories.entities.card;
 
 import java.sql.ResultSet;
+import java.util.Optional;
 
 import entities.Card;
+import enums.Status;
 import interfaces.factories.CardsFactory;
 
-public class rResulSetCardFactory implements CardsFactory {
+public class ResulSetCardFactory implements CardsFactory {
     private ResultSet resultSet;
 
     public Card createCard() throws Exception {
-        String id = resultSet.getString("id");
+        Integer id = resultSet.getInt("id");
         String title = resultSet.getString("title");
-        String status = resultSet.getString("status");
+        Status status = Status.valueOf(resultSet.getString("status"));
         boolean is_done = resultSet.getBoolean("is_done");
 
-        return new Card(id, title, status, is_done);
+        return new Card(Optional.of(id), title, status, is_done);
+    }
 }
